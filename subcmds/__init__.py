a0df9a88c41c968675c8af3d55500cd0d5349fd5
Rocky Gu <rocky.gu@broadcom.com>
2017-10-26 19:26:14 +0800

    add line numbers for file subcmds/__init__.py
    Signed-off-by: Rocky Gu <rocky.gu@broadcom.com>

--- subcmds/__init__.py
+++ subcmds/__init__.py
@@ -1,49 +1,49 @@
-#
-# Copyright (C) 2008 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-
-all_commands = {}
-
-my_dir = os.path.dirname(__file__)
-for py in os.listdir(my_dir):
-  if py == '__init__.py':
-    continue
-
-  if py.endswith('.py'):
-    name = py[:-3]
-
-    clsn = name.capitalize()
-    while clsn.find('_') > 0:
-      h = clsn.index('_')
-      clsn = clsn[0:h] + clsn[h + 1:].capitalize()
-
-    mod = __import__(__name__,
-                     globals(),
-                     locals(),
-                     ['%s' % name])
-    mod = getattr(mod, name)
-    try:
-      cmd = getattr(mod, clsn)()
-    except AttributeError:
-      raise SyntaxError('%s/%s does not define class %s' % (
-                         __name__, py, clsn))
-
-    name = name.replace('_', '-')
-    cmd.NAME = name
-    all_commands[name] = cmd
-
-if 'help' in all_commands:
-  all_commands['help'].commands = all_commands
+ 1 #
+ 2 # Copyright (C) 2008 The Android Open Source Project
+ 3 #
+ 4 # Licensed under the Apache License, Version 2.0 (the "License");
+ 5 # you may not use this file except in compliance with the License.
+ 6 # You may obtain a copy of the License at
+ 7 #
+ 8 #      http://www.apache.org/licenses/LICENSE-2.0
+ 9 #
+10 # Unless required by applicable law or agreed to in writing, software
+11 # distributed under the License is distributed on an "AS IS" BASIS,
+12 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+13 # See the License for the specific language governing permissions and
+14 # limitations under the License.
+15 
+16 import os
+17 
+18 all_commands = {}
+19 
+20 my_dir = os.path.dirname(__file__)
+21 for py in os.listdir(my_dir):
+22   if py == '__init__.py':
+23     continue
+24 
+25   if py.endswith('.py'):
+26     name = py[:-3]
+27 
+28     clsn = name.capitalize()
+29     while clsn.find('_') > 0:
+30       h = clsn.index('_')
+31       clsn = clsn[0:h] + clsn[h + 1:].capitalize()
+32 
+33     mod = __import__(__name__,
+34                      globals(),
+35                      locals(),
+36                      ['%s' % name])
+37     mod = getattr(mod, name)
+38     try:
+39       cmd = getattr(mod, clsn)()
+40     except AttributeError:
+41       raise SyntaxError('%s/%s does not define class %s' % (
+42                          __name__, py, clsn))
+43 
+44     name = name.replace('_', '-')
+45     cmd.NAME = name
+46     all_commands[name] = cmd
+47 
+48 if 'help' in all_commands:
+49   all_commands['help'].commands = all_commands
