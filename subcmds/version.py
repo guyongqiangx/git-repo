4e1fbf2a5095b1a52e8a9161051f125774ece540
Rocky Gu <rocky.gu@broadcom.com>
2019-01-03 07:53:47 +0800

    add comments for version.py
    Signed-off-by: Rocky Gu <rocky.gu@broadcom.com>

--- subcmds/version.py
+++ subcmds/version.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Copyright (C) 2009 The Android Open Source Project
 #
@@ -19,6 +20,27 @@ from command import Command, MirrorSafeCommand
 from git_command import git
 from git_refs import HEAD
 
+"""
+$ repo help version
+
+Summary
+-------
+Display the version of repo
+
+Usage: repo version
+
+Options:
+  -h, --help  show this help message and exit
+$
+$ repo version
+repo version v1.12.37
+       (from https://gerrit.googlesource.com/git-repo)
+repo launcher version 1.23
+       (from /home/ygu/bin/repo)
+git version 1.9.1
+Python 2.7.6 (default, Nov 13 2018, 12:45:42)
+[GCC 4.8.4]
+"""
 class Version(Command, MirrorSafeCommand):
   wrapper_version = None
   wrapper_path = None
@@ -29,16 +51,30 @@ class Version(Command, MirrorSafeCommand):
 %prog
 """
 
+  """
+  'repo version'命令中'version'操作的主函数。
+  """
   def Execute(self, opt, args):
     rp = self.manifest.repoProject
     rem = rp.GetRemote(rp.remote.name)
 
+    """
+    取得repo库HEAD引用指向的版本以及remote源的地址
+    """
     print('repo version %s' % rp.work_git.describe(HEAD))
     print('       (from %s)' % rem.url)
 
+    """
+    这访问main.py中设置的的全局变量Version, 而不是这里的Version类自身, 因为对于后者的访问应该是self
+
+    wrapper_path和wrapper_version在main.py中已经更新好了。
+    """
     if Version.wrapper_path is not None:
       print('repo launcher version %s' % Version.wrapper_version)
       print('       (from %s)' % Version.wrapper_path)
 
+    """
+    获取git工具的版本
+    """
     print(git.version().strip())
     print('Python %s' % sys.version)
